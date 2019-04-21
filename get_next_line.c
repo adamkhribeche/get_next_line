a1e684a48411c0a6593f4e00e84baa36280ab2a6
adamkhribeche <khribechadam@gmail.com>
2019-04-21 04:37:11 +0100

    add new line and print by recursion

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: nkhribec <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/16 13:57:56 by nkhribec          #+#    #+#             */
-/*   Updated: 2019/04/21 00:39:04 by nkhribec         ###   ########.fr       */
+/*   Updated: 2019/04/21 04:35:27 by nkhribec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,18 +22,23 @@
 */
 int		get_next_line(const int fd, char **line)
 {
-	char *p;
+	char		*p;
+	ssize_t		n;
 
 	p = *line;
-	read(fd, p++, 1);
-	while (*(p - 1) != '\n' && *(p - 1) != EOF)
+	n = read(fd, p, 1);
+	if (n == 0)
+		return (0);
+	if (*p == '\n')
+		return (1);
+	if (n == 1)
 	{
-		read(fd, p, 1);
-		p++;
+		(*line)++;
+		get_next_line(fd, line);
+		return (1);
 	}
-	//*p = '\0';
-	*line = p;
-	return (1);
+	else
+		return (-1);
 }
 
 int main(void)
