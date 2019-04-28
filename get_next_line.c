44de059d08dc0117b5f7134b539458911bf5b3fd
adamkhribeche <khribechadam@gmail.com>
2019-04-28 21:49:06 +0100

    leak fixed

--- get_next_line.c
+++ get_next_line.c
@@ -5,69 +5,80 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: nkhribec <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/04/16 13:57:56 by nkhribec          #+#    #+#             */
-/*   Updated: 2019/04/21 04:35:27 by nkhribec         ###   ########.fr       */
+/*   Created: 2019/04/21 05:30:42 by nkhribec          #+#    #+#             */
+/*   Updated: 2019/04/28 21:45:59 by nkhribec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-/*void	fct(void)
+static void	ft_append_line(char **line, char **s, const int fd)
 {
-	static int m = 0;
-	//m = 0;
-	m++;
-	printf("%d\n", m);
-}
-*/
-int		get_next_line(const int fd, char **line)
-{
-	char		*p;
-	ssize_t		n;
+	int  	len;
+	char	*to_free;
 
-	p = *line;
-	n = read(fd, p, 1);
-	if (n == 0)
-		return (0);
-	if (*p == '\n')
-		return (1);
-	if (n == 1)
+	len = 0;
+	to_free = s[fd];
+	while (s[fd][len] != '\n' && s[fd][len] != '\0')
+		len++;
+	if (s[fd][len] == '\n')
 	{
-		(*line)++;
-		get_next_line(fd, line);
-		return (1);
+		*line = ft_strsub(s[fd], 0, len);
+		s[fd] = ft_strdup(s[fd] + len + 1);
 	}
-	else
+	else if (s[fd][len] == '\0')
+	{
+		*line = ft_strsub(s[fd], 0, len);
+		s[fd] = ft_strdup(s[fd] + len);
+	}
+	free(to_free);
+}
+
+int			get_next_line(const int fd, char **line)
+{
+	static char		*s[4864];
+	char 			tmp[BUFF_SIZE + 1];
+	ssize_t			n;
+	char			*to_free;
+
+	if (fd < 0 || !line)
 		return (-1);
+	while ((n = read(fd, tmp, BUFF_SIZE)) > 0)
+	{
+		if (s[fd] == NULL)
+			s[fd] = ft_memalloc(sizeof(char));
+		to_free = s[fd];
+		tmp[n] = '\0';
+		s[fd] = ft_strjoin(s[fd], tmp);
+		free(to_free);
+		if (ft_strchr(s[fd], '\n'))
+			break;
+	}
+	if (n == -1)
+		return (-1);
+	if (n == 0 && (s[fd] == NULL || s[fd][0] == '\0'))
+	{
+		free(s[fd]);
+		return (0);
+	}
+	ft_append_line(line, s, fd);
+	return (1);
 }
 
 int main(void)
 {
 	int		fd;
 	char	*line;
-	char	*g;
+	int 	n;
 
-	line  = (char*)ft_memalloc(BUFF_SIZE * sizeof(*line));
-	g = line;
 	fd = open("adam", O_RDONLY);
-	get_next_line(fd, &line);
-	printf("%s", g);
-	printf("-----------------\n");
-	get_next_line(fd, &line);
-	printf("%s", g);
-	printf("-----------------\n");
-	get_next_line(fd, &line);
-	printf("%s", g);
-	printf("-----------------\n");
-	get_next_line(fd, &line);
-	printf("%s", g);
-	printf("-----------------\n");
-	get_next_line(fd, &line);
-	printf("%s", g);
-	printf("-----------------\n");
-	get_next_line(fd, &line);
-	printf("%s", g);
-	printf("-----------------\n");
+	while ((n = get_next_line(fd, &line)) > 0)
+	{
+		printf("%s", line);
+		printf("\n-------n = %d----------\n",n);
+		ft_strdel(&line);
+	}
+	ft_strdel(&line);
 	close(fd);
 	return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,19 +6,27 @@
 /*   By: nkhribec <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/16 13:58:38 by nkhribec          #+#    #+#             */
-/*   Updated: 2019/04/19 18:44:22 by nkhribec         ###   ########.fr       */
+/*   Updated: 2019/04/28 21:23:35 by nkhribec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE
-# define GET_NEXT_LINE
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
 # include <stdlib.h>
-# include "libft.h"
+# include "libft/libft.h"
 # include <unistd.h>
 # include <stdio.h>
 # include <fcntl.h>
 
-# define BUFF_SIZE 1000
-int		get_next_line(const int fd, char **line);
+# define BUFF_SIZE 10000
+
+int					get_next_line(const int fd, char **line);
+
+/*typedef struct		s_list
+{
+	int				fd;
+	char			*str;
+	struct s_list	*next;
+}					t_list;*/
 #endif
